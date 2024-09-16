2cc6e4496671475a881e306fed8f10177b7219a0
gitXite <daniel.halaas@gmail.com>
2024-09-16 10:23:46 +0200

    Added tickrate

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        pygame.display.flip()
+        pygame.display.flip() # Updates the screen after each iteration
+        dt = clock.tick(60) / 1000 # ticks at 60hz, converts delta time from ms to sec
 
 if __name__ == "__main__":
     main()
