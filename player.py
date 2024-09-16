cd97d00bfbc5f85f47d41741d55c6505bee9c7f0
gitXite <daniel.halaas@gmail.com>
2024-09-16 12:45:17 +0200

    Fixed bugs

--- player.py
+++ player.py
@@ -5,8 +5,8 @@ from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
-        super().__init__(PLAYER_RADIUS)
-        rotation = 0
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 180
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -19,4 +19,3 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), width=2)
 
-        
