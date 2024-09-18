e33bc440a70acb419a25d9143e27d26955af1a62
gitXite <daniel.halaas@gmail.com>
2024-09-18 11:48:36 +0200

    Check for collisions method added

--- circleshape.py
+++ circleshape.py
@@ -12,10 +12,17 @@ class CircleShape(pygame.sprite.Sprite):
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0,0)
 
-        def draw(self, screen):
-            pass
 
-        def update(self, dt):
-            pass
+    def draw(self, screen):
+        pass
 
-        
+
+    def update(self, dt):
+        pass
+
+
+    def check_for_collisions(self, CircleShape):
+        distance = pygame.math.Vector2.distance_to(self.position, CircleShape.position)
+        if distance <= self.radius:
+            return True
+        return False
